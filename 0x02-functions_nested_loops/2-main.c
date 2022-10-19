2d283064ee769738c3706dff10c60ee409d80c13
augustabube <ibeaugustabube@gmail.com>
2022-10-19 10:30:43 -0700

    Print lowercase

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Description: print_alphabet_x10 using _putchar
+ *
+ * Return: Always 0. 
+ */
+int main(void)
+{
+	print_alphabet_x10();
+	return (0);
+}
