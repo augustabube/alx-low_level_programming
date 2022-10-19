77123ee271d1258577f06618cbea759b9d901a0b
augustabube <ibeaugustabube@gmail.com>
2022-10-18 17:26:18 -0700

    Print alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * Description: print_alphabet - using _putchar
+ *
+ * Return: 0 (Success)
+ */
+void print_alphabet(void)
+{
+
+	int i;
+
+	for (i = 'a'; i <= 'z'; i++)
+
+		_putchar(i);
+
+	_putchar('\n');
+}
