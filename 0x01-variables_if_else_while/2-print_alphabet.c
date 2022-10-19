3a9cc6158a941e98e81ac0a5ad35b58479e37b68
augustabube <ibeaugustabube@gmail.com>
2022-10-18 18:47:48 -0700

    Last digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - print a-z using putchar
+ *
+ * Return: 0 (Success)
+ */
+int lower_y(tolower(y)
+		{
+		return tolower(y);
+int main(void) 
+{
+	for (int y = 'A'; y <= 'Z'; y++)
+	{
+		int lower_y = tolower(y);
+		putchar(lower_y);
+	}
+	return (0);
+}
