e90f69a498050da9499a1cb421656d86a88cdc60
augustabube <ibeaugustabube@gmail.com>
2022-10-19 14:11:25 -0700

    Last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_last_digit - print the last digit of number
+ * @n: int data type
+ * Return: return value to last digit
+ */
+int print_last_digit(int n)
+{
+	int ld;
+
+	if (n < 0)
+	{
+		ld = -1 * (n % 10);
+		_putchar(ld + '0');
+		return (ld);
+	}
+	else
+	{
+		ld = n % 10;
+		_putchar(ld + '0');
+		return (ld);
+	}
+}
+
