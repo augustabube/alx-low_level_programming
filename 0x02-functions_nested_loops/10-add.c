e25428a37054141265339e4a3c1f9793f3c06eea
augustabube <ibeaugustabube@gmail.com>
2022-10-19 16:42:38 -0700

    Add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - two integers
+ * @x: int data type
+ * @y: int data type
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	int solve = (x + y);
+
+	return (solve);
+}
