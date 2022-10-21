7a97957dc508a8dc2688a824684fb6d6a5905f67
augustabube <ibeaugustabube@gmail.com>
2022-10-21 13:00:27 -0700

    Diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,14 +7,18 @@
  */
 void print_diagonal(int n)
 {
-	int i, j;
+	int i = 0, j;
 
-	for (i = 0; i < n; i++)
+	if (n > 0)
+	{
+	for (; i < n; i++)
 	{
 		for (j = 0; j < n; j++)
-		{
-			_putchar(' ');
-		}
+		_putchar(' ');
+		_putchar(92);
 		_putchar('\n');
 	}
+	}
+	else
+		_putchar('\n');
 }
