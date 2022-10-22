cbd591d3a83ee6cd0c0b941bab8439651856c10d
augustabube <ibeaugustabube@gmail.com>
2022-10-21 17:00:43 -0700

    Task,1, 2,3

---
+++ 0x03-debugging/3-main_b.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - take a date and print how many days are left
+ * leap years int consideration
+ * Return: 0
+ */
+int main(void)
+{
+	int month;
+	int day;
+	int year;
+	
+	month = 02;
+	day = 29;
+	year = 2000;
+	
+	printf("Date: %02d/%02d/%04d\n", month, day, year);
+	day = convert_day(month, day);
+
+	print_remaining_days(month, day, year);
+	return (0);
+}
