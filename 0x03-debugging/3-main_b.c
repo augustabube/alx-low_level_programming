20596af8d225e6a7e939a966db4d17207e0ca137
augustabube <ibeaugustabube@gmail.com>
2022-10-21 18:54:26 -0700

    3 main

--- 0x03-debugging/3-main_b.c
+++ 0x03-debugging/3-main_b.c
@@ -10,11 +10,11 @@ int main(void)
 	int month;
 	int day;
 	int year;
-	
+
 	month = 02;
 	day = 29;
 	year = 2000;
-	
+
 	printf("Date: %02d/%02d/%04d\n", month, day, year);
 	day = convert_day(month, day);
 
