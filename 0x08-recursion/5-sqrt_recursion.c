803a101494603c32c91ff42c05977ca232c47165
augustabube <ibeaugustabube@gmail.com>
2022-11-16 09:36:07 -0800

    squareroot of number

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,11 +23,11 @@ int power_operation(int n, int a)
  */
 int _sqrt_recursion(int n)
 {
-	if (n < o)
+	if (n < 0)
 		return (-1);
 	if (n == 0)
 		return (0);
 	if (n == 1)
 		return (1);
-	return (power_recursion(n, 2));
+	return (power_operation(n, 2));
 }
